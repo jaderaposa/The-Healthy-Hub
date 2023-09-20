b70717dbecf3e42645ffdbf6c40a3e023f35a15b
jaderaposa <jaduya678@gmail.com>
2023-09-20 22:34:41 +0800

    ???

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-   
+    <script src="{{ asset('js/app.js') }}"></script>
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
     <title>Document</title>
 </head>
 <body>
