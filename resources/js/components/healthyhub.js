b5e6b2a280b79e9f6348ed5a7c87de505858258d
jaderaposa <jaduya678@gmail.com>
2023-09-21 23:34:20 +0800

    asdasdsadsa

---
+++ resources/js/components/healthyhub.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const Label = () => {
+  return (
+    <div className="label">
+      <div className="text-wrapper">The Healthy Hub</div>
+    </div>
+  );
+};
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Authentication routes (Auth::routes() already includes these)
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Resourceful route for posts
+Route::resource('posts', PostController::class);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
