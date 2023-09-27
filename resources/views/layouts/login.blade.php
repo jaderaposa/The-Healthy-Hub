325fd805133b03689ff4bdad63ac3abec3a5fa45
Jaded <jaduya678@gmail.com>
2023-09-27 09:31:02 +0800

    social medias

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -21,6 +21,40 @@
         <div class="text-wrapper">The Healthy Hub</div>
         <p class="div">“Eat for your future, not just your taste buds”</p>
     </div>
+    <div class="social-media">
+        <div class="group">
+            <div>
+            <p class="text-wrapper">Follow us through our social media</p>
+            </div>
+            <div class="icons">
+                <div>
+                    <img class="facebook-app-symbol" src="img/facebook-app-symbol-1.png" />
+                </div>
+                <div>
+                    <img class="twitter" src="img/twitter-1.png" />
+                </div>
+                <div>
+                    <img class="reddit" src="img/reddit-1.png" />
+                </div>
+                <div>
+                    <img class="instagram" src="img/instagram-1.png" />
+                </div>
+            </div>
+
+            <div class="div">
+                <img class="line" src="img/line-5.svg" />
+                <img class="img" src="img/line-6.svg" />
+                <img class="line-2" src="img/line-7.svg" />
+                <div class="text-wrapper-2">Home</div>
+                <div class="text-wrapper-3">Products</div>
+                <div class="text-wrapper-4">Company</div>
+                <div class="text-wrapper-5">Blog</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
     
 </body>
 </html>
